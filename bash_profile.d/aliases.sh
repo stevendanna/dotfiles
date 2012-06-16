1f519cc8159c968b260dd9a083d13189c371a07a
Steven Danna <StevenSDanna@gmail.com>
2012-06-16 14:31:11 -0700

    Add rlwrap to ed alias

--- bash_profile.d/aliases.sh
+++ bash_profile.d/aliases.sh
@@ -2,7 +2,7 @@ source ~/.bash_profile.d/functions.sh
 
 alias be='bundle exec'
 alias bi='bundle install'
-alias ed='ed -p "ed> "'
+alias ed='rlwrap ed -p "ed> "'
 alias ec='emacsclient -nw'
 
 # OS X
