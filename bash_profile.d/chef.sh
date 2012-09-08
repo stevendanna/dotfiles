5c3d562a63c184123ee1b1a2610ca8e8c1643f3f
Steven Danna <steve@opscode.com>
2012-09-07 22:14:22 -0700

    Add helper functions for working with Chef-related tickets.

---
+++ bash_profile.d/chef.sh
@@ -0,0 +1,23 @@
+source ~/.bash_profile.d/functions.sh
+
+if is_os_x; then
+    OPENCOMMAND="open"
+else
+    OPENCOMMAND="gnome-open"
+fi
+
+CHEF() {
+    $OPENCOMMAND http://tickets.opscode.com/browse/CHEF-$1
+}
+
+OHAI() {
+    $OPENCOMMAND http://tickets.opscode.com/browse/OHAI-$1
+}
+
+KNIFE_EC2() {
+    $OPENCOMMAND http://tickets.opscode.com/browse/KNIFE_EC2-$1
+}
+
+OC() {
+    $OPENCOMMAND http://tickets.opscode.com/browse/KNIFE_EC2-$1
+}
